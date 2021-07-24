5337e505ec891a2c56a2e394582baeffd3882f7d
strobosaur <strobosaur@gmail.com>
2021-07-24 15:13:13 +0200

    GP Stick input optimized, camera smooth sync with player movement

---
+++ Assets/Scripts/Player/CameraControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // CAMERA VARIABLES
+    public Transform target;
+    public Vector3 newPos;
+    public float smoothing = 0.25f;
+    public float camTargetDist;
+    public float playercamTargetDist;
+    public float camPlayerDist;
+    public GameObject player;
+    public Rigidbody2D playerRb;
+
+    // START
+    void Start() 
+    {
+        player = GameObject.Find("Droid");
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // LATE UPDATE
+    void LateUpdate()
+    {      
+        MoveCamera();
+    }
+
+    // MOVE CAMERA
+    void MoveCamera()
+    {        
+        if(transform.position != target.position)
+        {
+            // CALCULATE DISTANCES
+            playercamTargetDist = Vector3.Distance(target.transform.position, playerRb.transform.position);
+            camTargetDist = Vector2.Distance(
+                new Vector2(transform.position.x, transform.position.y), 
+                new Vector2(target.transform.position.x, target.transform.position.y));
+            camPlayerDist = Vector2.Distance(
+                new Vector2(transform.position.x, transform.position.y), 
+                new Vector2(playerRb.transform.position.x, playerRb.transform.position.y));
+
+            // IF TARGET AND CAM POSITION IS TOO CLOSE TO PLAYER, SET POSITION TO PLAYERS
+            if ((Mathf.FloorToInt(playercamTargetDist * 16f) < 1)
+            && (Mathf.FloorToInt(camPlayerDist * 16f) < 1)) {
+                transform.position = new Vector3(playerRb.transform.position.x, playerRb.transform.position.y, transform.position.z);
+            } else {
+                Vector3 camTarget = Vector3.Lerp(transform.position, target.position, smoothing);
+                //Vector3 camPlayerDiff = transform.position - playerRb.transform.position;
+
+                // IF CAM IS ALMOST AT TARGET, SET POSITION TO TARGETS
+                if (Mathf.FloorToInt(camTargetDist * 16f) > 1){
+                    Vector2 camPlayerDiff = new Vector2(camTarget.x, camTarget.y) 
+                        - new Vector2(playerRb.transform.position.x, playerRb.transform.position.y);
+                    float newX = playerRb.transform.position.x + ((float)(Mathf.RoundToInt(camPlayerDiff.x * 16f)) / 16);
+                    float newY = playerRb.transform.position.y + ((float)(Mathf.RoundToInt(camPlayerDiff.y * 16f)) / 16);
+                    newPos = new Vector3(newX, newY, transform.position.z);
+                    transform.position = newPos; 
+                } else {
+                    Vector2 targetPlayerDiff = new Vector2(target.position.x, target.position.y) 
+                        - new Vector2(playerRb.transform.position.x, playerRb.transform.position.y);
+                    float newX = playerRb.transform.position.x + ((float)(Mathf.RoundToInt(targetPlayerDiff.x * 16f)) / 16);
+                    float newY = playerRb.transform.position.y + ((float)(Mathf.RoundToInt(targetPlayerDiff.y * 16f)) / 16);
+                    newPos = new Vector3(newX, newY, transform.position.z);
+                    transform.position = newPos; 
+                    //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+                }
+            }            
+        }  
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,47 +9,66 @@ namespace Game.Controls
 {
     public class PlayerController : MonoBehaviour
     {
+        // GAMEPAD
         Gamepad gamepad = Gamepad.current;
+        public Vector2 LSinp;
+        public Vector2 LSdir;
+        public Vector2 RSinp;
+        public Vector2 RSdir;
+        private float LSDeadzone = 0.2f;
+        private float RSDeadzone = 0.2f;
 
         // CAMERA TARGET
         public GameObject camTarget;
-        public float camSpd = 0.04f;
+        public float camSpd = 0.075f;
         public float camDist = 0.5f;
 
         // PLAYER VARIABLES
-
         public float moveSpd = 2.5f;
         public float moveMag;
         public float moveLen;
         public Vector2 movement;
+        public Vector2 moveTo;
+        public Vector2 moveToDir;
         public Vector2 moveDir;
+        public Vector2 faceDir;
+
         public Animator animator;
+        public float animSpdFactor = 0.5f;
+
         public Rigidbody2D rb;
 
         // CROSSHAIR VARIABLES        
         public GameObject crossHair;
-        private Vector2 targetPos;
-        private Vector2 currentPos;
+        public Vector2 crossTargetPos;
+        public Vector2 crossCurrentPos;
+        public float crossTargetDist;
         public float crossDistance = 9f;
-        public float crossSpd;
+        public float crossSpd = 0.25f;
+        public float crossOpacity;
         private SpriteRenderer sprite; 
         
+        // UPDATE
         void Update()
         {
             ProcessInput();
         }
 
+        // FIXED UPDATE
         void FixedUpdate() 
         {
             Move();
             MoveCrosshair();
-        }
-
-        void LateUpdate()
-        {
             MoveCamTarget();
         }
 
+        // LATE UPDATE
+        void LateUpdate()
+        {
+
+        }
+
+        // PROCESS INPUT
         void ProcessInput()
         {
             if (gamepad == null)
@@ -57,60 +76,93 @@ namespace Game.Controls
                 return;
             }
 
-            Vector3 LSTinp = gamepad.leftStick.ReadValue();
+            // LEFT STICK INPUT
+            LSinp = gamepad.leftStick.ReadValue();
+            if(LSinp.magnitude < LSDeadzone){
+                LSinp = new Vector2(0f, 0f);
+            } else {
+                LSinp = Vector2.ClampMagnitude(LSinp, 1.0f);
+            }
 
-            moveDir = new Vector2(LSTinp.x, LSTinp.y);
-            moveDir = Vector2.ClampMagnitude(moveDir, 1.0f);
-            moveLen = moveDir.magnitude;
+            LSdir = LSinp.normalized;
+
+            // RIGHT STICK INPUT
+            RSinp = gamepad.rightStick.ReadValue();
+            if(RSinp.magnitude < RSDeadzone){
+                RSinp = new Vector2(0f, 0f);
+            } else {
+                RSinp = Vector2.ClampMagnitude(RSinp, 1.0f);
+            }
+
+            RSdir = RSinp.normalized;
         }
 
+        // MOVE PLAYER
         void Move()
         {
-            movement = Vector2.Lerp(movement, moveDir, Globals.G_INERTIA);
-            rb.velocity = movement * moveSpd;            
-            moveMag = movement.magnitude;
-            
-            animator.SetFloat("horizontal", movement.x);
-            animator.SetFloat("vertical", movement.y);
+            /*moveToDir = Vector2.ClampMagnitude(LSinp, 1.0f);
+            if (moveToDir.magnitude < LSDeadzone)
+            {
+                moveToDir = new Vector2(0f, 0f);
+            }*/
+
+            moveLen = LSinp.magnitude;
+            moveTo = LSinp * moveSpd;
+            movement = Vector2.Lerp(movement, moveTo, Globals.G_INERTIA);
+
+            if (movement.magnitude > 0.01f) {
+                rb.velocity = movement;
+                moveDir = movement.normalized;
+                moveMag = movement.magnitude;
+            } else {
+                movement = new Vector2(0f, 0f);
+                rb.velocity = new Vector2(0f, 0f);
+                moveMag = 0f;
+            }
+
+            // ANIMATOR
+            animator.SetFloat("horizontal", moveDir.x);
+            animator.SetFloat("vertical", moveDir.y);
             animator.SetFloat("magnitude", moveMag);
-            animator.speed = moveMag;
+            animator.speed = moveMag * animSpdFactor;
         }
 
+        // MOVE CROSSHAIR
         private void MoveCrosshair()
         {
-            // GET GAMEPAD INPUT
-            Vector3 RSTinp = gamepad.rightStick.ReadValue();
-            Vector2 RSTdir = new Vector2(RSTinp.x, RSTinp.y);
-            RSTdir = Vector2.ClampMagnitude(RSTdir, 1.0f);
-            float RSTlen = RSTdir.magnitude;
-
             // CHECK FOR RIGHT STICK ACTION
-            if(RSTinp.magnitude > 0.1)
+            if(RSinp.magnitude > RSDeadzone)
             {
                 // CREATE NEW TARGET
-                Vector2 crossDir = new Vector2(RSTdir.x, RSTdir.y);
-                targetPos = new Vector2(rb.position.x, rb.position.y) + (crossDistance * crossDir);
+                //Vector2 crossDir = new Vector2(RSinp.x, RSinp.y);
+                crossTargetPos = new Vector2(rb.position.x, rb.position.y) + (crossDistance * RSinp);
+
+                // PLAYER FACING
+                faceDir = RSinp.normalized;
             }
             else
             {
                 // RETURN TO PLAYER
-                targetPos = new Vector2(rb.position.x, rb.position.y);
+                crossTargetPos = new Vector2(rb.position.x, rb.position.y);
+                faceDir = moveDir;
             }
 
             // GET DISTANCE TO TARGET
-            float targetDist = Vector3.Distance(new Vector2(crossHair.transform.position.x, crossHair.transform.position.y), targetPos);
+            crossTargetDist = Vector2.Distance(
+                new Vector2(crossHair.transform.position.x, crossHair.transform.position.y), 
+                crossTargetPos);
 
             // SMOOTH CROSSHAIR MOVEMENT
-            if (Mathf.CeilToInt(targetDist * 16) > 2)
+            if (Mathf.CeilToInt(crossTargetDist * 16) > 2)
             {
-                //crossHair.transform.position = Vector2.MoveTowards(crossHair.transform.position, targetPos, (0.75f));
-                crossHair.transform.position = Vector2.Lerp(crossHair.transform.position, targetPos, (0.25f));
+                //crossHair.transform.position = Vector2.MoveTowards(crossHair.transform.position, crossTargetPos, (0.75f));
+                crossHair.transform.position = Vector2.Lerp(crossHair.transform.position, crossTargetPos, crossSpd);
             }
 
             // ADJUST CROSSHAIR OPACITY BASED ON DISTANCE TO PLAYER
-            float opacity = (Vector3.Distance(crossHair.transform.position, rb.transform.position) - 2) / 5;
+            crossOpacity = (Vector3.Distance(crossHair.transform.position, rb.transform.position) - 2) / 5;
             sprite = crossHair.GetComponent<SpriteRenderer>();
-            sprite.color = new Color(1,1,1,opacity);            
+            sprite.color = new Color(1,1,1,crossOpacity);            
         }
 
         private void MoveCamTarget()
